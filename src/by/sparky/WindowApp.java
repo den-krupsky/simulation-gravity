eb11127a3c3bea0173d8a1dca5b41034e5b81cd4
sparky <spurkky.sined@gmail.com>
2020-02-29 20:37:17 +0300

    Refactoring

--- src/by/sparky/WindowApp.java
+++ src/by/sparky/WindowApp.java
@@ -5,29 +5,42 @@ import java.awt.*;
 import java.util.Random;
 
 public class WindowApp {
+    private final JFrame frame;
 
-    public static void main(String[] args) {
-        new WindowApp().start();
+    public WindowApp(JFrame frame) {
+        this.frame = frame;
     }
 
-    private void start() {
-        JFrame frame = new JFrame();
-        frame.setSize(1366, 768);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
+    private PhysicObject[] generate() {
         PhysicObject[] physicObjects = new PhysicObject[30];
         Random random = new Random();
         Random random1 = new Random();
         for (int i = 0; i < physicObjects.length; i++) {
             physicObjects[i] = new PhysicObject(10000.0d + random.nextDouble() * 20000, random.nextDouble() * random1.nextInt(1366), random1.nextDouble() * random.nextInt(768));
         }
+        return physicObjects;
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Gravitation");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
+
+        WindowApp windowApp = new WindowApp(frame);
+        windowApp.start();
+    }
+
+    private void start() {
+        PhysicObject[] physicObjects = generate();
+
         PhysicsEngine physicsEngine = new PhysicsEngine();
         physicsEngine.setPhysicObject(physicObjects);
         physicsEngine.setInteractions(new Gravity());
 
-
         ViewPhysicObject view = new ViewPhysicObject(physicObjects);
-        view.setSize(1366, 768);
+
+        view.setSize(frame.getWidth(), frame.getHeight());
         frame.add(view, BorderLayout.CENTER);
         frame.setVisible(true);
 
