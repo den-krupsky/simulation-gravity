0a07b2e0c0f2175c3a3c928ea62e5e2dc308ac0c
sparky <spurkky.sined@gmail.com>
2020-03-06 16:43:20 +0300

    Clean up

--- src/by/sparky/PhysicObject.java
+++ src/by/sparky/PhysicObject.java
@@ -6,15 +6,11 @@ public class PhysicObject implements Point {
     protected double y;
     protected double vx;
     protected double vy;
-    protected Vector2D point;
-    protected Vector2D velocity;
 
     PhysicObject(double mass, double x, double y) {
         this.mass = mass;
         this.x = x;
         this.y = y;
-        point = new Vector2D(x, y);
-        velocity = new Vector2D(0, 0);
     }
 
     @Override
