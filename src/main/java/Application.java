917dcbee285f002473cbcd038ec86f1cc6611752
sparky <spurkky.sined@gmail.com>
2020-03-07 02:44:08 +0300

    Add exception

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -13,6 +13,7 @@ public class Application {
         DisplayMode dm = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode();
 
         int count = args.length > 0 ? Integer.parseInt(args[0]) : 100;
+        if (count < 1) throw new IllegalArgumentException("Objects count must be greater than 1: " + count);
         SimulationService simulationService = initialize(count, dm.getWidth(), dm.getHeight());
         simulationService.run();
     }
